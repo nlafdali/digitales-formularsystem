3eae1c12648bfe6e9bb4ac3a98b9b078ccb46377
nlafdali <lafdali.nadia7@gmail.com>
2025-10-06 10:04:35 +0200

    Initial project setup

---
+++ src/main/java/com/example/formsys/FormsysApplication.java
@@ -0,0 +1,13 @@
+package com.example.formsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/formsys/FormsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
